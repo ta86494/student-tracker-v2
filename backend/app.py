75f2ccf8f411dba2f0b2114d4220b59a8411cece
ta86494 <2021wa86494@wilp.bits-pilani.ac.in>
2025-11-03 12:09:36 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -653,4 +653,8 @@ def create_app():
 
 if __name__ == '__main__':
     create_app()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',8000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)), debug=True)
+else:
+    # This branch runs when Azure starts gunicorn
+    create_app()
+
